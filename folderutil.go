e31619e14f795f602a0e69f013e7208e09086826
Mzack9999 <mzack9999@protonmail.com>
2021-05-19 21:04:22 +0200

    adding dumb file list helper

---
+++ folderutil.go
@@ -0,0 +1,24 @@
+package folderutil
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func GetFiles(root string) ([]string, error) {
+	var matches []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		matches = append(matches, path)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
+}
